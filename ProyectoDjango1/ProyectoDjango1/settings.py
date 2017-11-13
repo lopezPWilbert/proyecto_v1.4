4038474023eed6629a07b2d083f42116c0cc45ba
Wilbert Lopez <wilbert.lopez17@tectijuana.edu.mx>
2017-11-12 20:13:41 -0800

    Guarda id en form de img y vid, pero no las rutas.

--- ProyectoDjango1/ProyectoDjango1/settings.py
+++ ProyectoDjango1/ProyectoDjango1/settings.py
@@ -168,8 +168,10 @@ LOGIN_REDIRECT_URL = reverse_lazy("mapa_view")
 LOGOUT_URL = reverse_lazy("login")
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-#MEDIA_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['media']))
+#Linux
+#MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+#lo otro
+MEDIA_ROOT = posixpath.join(*(BASE_DIR.split(os.path.sep) + ['media']))
 
 
 ####  SOLO SIRVE PARA DESARROLLO Y PRUEBAS  #######
